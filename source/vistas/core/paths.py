d63c2c759ab032b61ec67398d5911edc85ebb1dc
nikmolnar <nik.molnar@consbio.org>
2017-04-22 12:19:49 -0700

    Update paths for Windows

--- source/vistas/core/paths.py
+++ source/vistas/core/paths.py
@@ -2,10 +2,18 @@ import os
 
 import wx
 
+from vistas.core.utils import get_platform
+
 
 def get_builtin_plugins_directory():
-    return os.path.join(os.path.dirname(wx.StandardPaths.Get().ExecutablePath), '../plugins')
+    if get_platform() == 'windows':
+        return os.path.join(os.getcwd(), '..', 'plugins')
+    else:
+        return os.path.join(os.path.dirname(wx.StandardPaths.Get().ExecutablePath), '..', 'plugins')
 
 
 def get_resources_directory():
-    return os.path.join(os.path.dirname(wx.StandardPaths.Get().ExecutablePath), '../resources')
+    if get_platform() == 'windows':
+        return os.path.join(os.getcwd(), '..', 'resources')
+    else:
+        return os.path.join(os.path.dirname(wx.StandardPaths.Get().ExecutablePath), '..', 'resources')
