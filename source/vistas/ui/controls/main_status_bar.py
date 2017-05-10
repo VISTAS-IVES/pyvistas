9b9dac4cf4d2b9c4f46a8bcbc9c360af80688ff4
Taylor Mutch <taylormutch@gmail.com>
2017-05-10 14:46:04 -0700

    Added Task awareness to MainStatusBar.

--- source/vistas/ui/controls/main_status_bar.py
+++ source/vistas/ui/controls/main_status_bar.py
@@ -1,3 +1,4 @@
+from vistas.core.task import Task
 import wx
 
 
@@ -6,14 +7,24 @@ class MainStatusBar(wx.StatusBar):
         super().__init__(parent, id)
         self.SetFieldsCount(2, [70, -1])
         self.SetStatusText("Idle", 1)
-        self.gauge = wx.Gauge(self, wx.ID_ANY, 100, wx.Point(5, 3), wx.Size(60, self.GetSize().Get()[1] - 6))
-        # Todo: Start?
+        self.gauge = None
+        self.timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.OnNotify)
+        self.timer.Start(100, True)
 
-    def Notify(self):
-        # Todo: Get newest task?
-        task = None
-        status = "Idle"
-
-        # Todo: activate guage or turn off
-
-        # Todo: Start?
+    def OnNotify(self, event):
+        task = Task.tasks[-1] if len(Task.tasks) else None
+        if task is not None and task.status not in [Task.STOPPED, Task.COMPLETE]:
+            if self.gauge is None:
+                self.gauge = wx.Gauge(self, wx.ID_ANY, 100, wx.Point(5, 3), wx.Size(60, self.GetSize().Get()[1] - 6))
+            self.SetStatusText(task.name, 1)
+            if task.status is Task.RUNNING:
+                self.gauge.SetValue(task.percent)
+            else:
+                self.gauge.Pulse()
+        else:
+            self.SetStatusText("Idle", 1)
+            if self.gauge is not None:
+                self.gauge.Destroy()
+                self.gauge = None
+        self.timer.Start(100, True)
