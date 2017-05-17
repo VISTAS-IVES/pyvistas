7c1aacc8b28f2a085bd8cdd284f6dc8f0ba78771
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2017-05-17 10:28:05 -0700

    Add logging and exception handling

--- source/vistas/ui/app.py
+++ source/vistas/ui/app.py
@@ -1,18 +1,36 @@
 import asyncio
 import sys
 
+import logging
+import traceback
+
 import wx
 
 from vistas.ui.controllers.app import AppController
+from vistas.ui.windows.exception_dialog import ExceptionDialog
+
+logger = logging.getLogger(__name__)
 
 
 class App(wx.App):
-    def __init__(self):
-        super().__init__()
+    _global_app = None
 
+    @classmethod
+    def get(cls):
+        if cls._global_app is None:
+            cls._global_app = App()
+        return cls._global_app
+
+    def __init__(self):
         self.app_controller = None
 
+        super().__init__()
+
     def OnInit(self):
+        logger.debug('VISTAS starting...')
+
+        sys.excepthook = exception_hook
+
         if sys.platform == 'win32':
             asyncio.set_event_loop(asyncio.ProactorEventLoop())
         else:
@@ -20,4 +38,15 @@ class App(wx.App):
 
         self.app_controller = AppController()
 
+        logger.debug('VISTAS started')
+
         return True
+
+
+def exception_hook(exc_type, value, trace):
+    exception_message = ''.join(traceback.format_exception(exc_type, value, trace))
+    logger.error('Unhandled exception\n{}'.format(exception_message))
+
+    dialog = ExceptionDialog(App.get().app_controller.main_window, exception_message)
+    dialog.ShowModal()
+    dialog.Destroy()
---
+++ source/vistas/ui/windows/exception_dialog.py
@@ -0,0 +1,35 @@
+import wx
+
+
+class ExceptionDialog(wx.Dialog):
+    def __init__(self, parent, message):
+        super().__init__(parent, wx.ID_ANY)
+
+        self.SetSize((600, 400))
+
+        main_panel = wx.Panel(self, wx.ID_ANY)
+        label = wx.StaticText(main_panel, wx.ID_ANY, 'An unhandled exception occurred:')
+        text = wx.TextCtrl(main_panel, wx.ID_ANY, message, style=wx.TE_MULTILINE | wx.TE_READONLY | wx.HSCROLL)
+
+        button = wx.Button(main_panel, wx.ID_ANY, 'Ok')
+        button.SetDefault()
+
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(main_sizer)
+
+        main_sizer.Add(main_panel, 1, wx.EXPAND)
+
+        main_panel_sizer = wx.BoxSizer(wx.VERTICAL)
+        main_panel.SetSizer(main_panel_sizer)
+
+        main_panel_sizer.Add(label, 0, wx.LEFT | wx.TOP | wx.RIGHT, 5)
+        main_panel_sizer.Add(text, 1, wx.EXPAND | wx.ALL, 5)
+        main_panel_sizer.Add(button, 0, wx.ALIGN_RIGHT | wx.LEFT | wx.BOTTOM | wx.RIGHT, 5)
+
+        button.Bind(wx.EVT_BUTTON, self.OnOk)
+
+        self.Layout()
+        self.CenterOnParent()
+
+    def OnOk(self, event):
+        self.Close()
