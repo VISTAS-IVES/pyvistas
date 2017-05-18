ed91cfefa2173bd252c64dfd56f7621dd87e747d
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2017-05-18 11:48:08 -0700

    Fix exception handling

--- source/vistas/ui/app.py
+++ source/vistas/ui/app.py
@@ -11,6 +11,8 @@ from vistas.ui.windows.exception_dialog import ExceptionDialog
 
 logger = logging.getLogger(__name__)
 
+HandleExceptionEvent, EVT_HANDLE_EXCEPTION = wx.lib.newevent.NewEvent()
+
 
 class App(wx.App):
     _global_app = None
@@ -25,6 +27,8 @@ class App(wx.App):
         self.app_controller = None
 
         super().__init__()
+
+        self.Bind(EVT_HANDLE_EXCEPTION, self.OnHandleException)
 
     def OnInit(self):
         logger.debug('VISTAS starting...')
@@ -42,11 +46,14 @@ class App(wx.App):
 
         return True
 
+    def OnHandleException(self, event):
+        dialog = ExceptionDialog(self.app_controller.main_window, event.message)
+        dialog.ShowModal()
+        dialog.Destroy()
+
 
 def exception_hook(exc_type, value, trace):
     exception_message = ''.join(traceback.format_exception(exc_type, value, trace))
     logger.error('Unhandled exception\n{}'.format(exception_message))
 
-    dialog = ExceptionDialog(App.get().app_controller.main_window, exception_message)
-    dialog.ShowModal()
-    dialog.Destroy()
+    wx.PostEvent(App.get(), HandleExceptionEvent(message=exception_message))
