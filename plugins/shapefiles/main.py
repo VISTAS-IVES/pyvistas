222ff88cddeb9ec6fea4f8b09084e28b55c51d1a
Taylor Mutch <taylormutch@gmail.com>
2017-05-29 16:48:35 -0700

    Basic polygon support in shapefile plugin.

--- plugins/shapefiles/main.py
+++ plugins/shapefiles/main.py
@@ -1,5 +1,7 @@
 import os
 import fiona
+from pyproj import Proj
+from shapely.geometry import Point, Polygon
 
 from vistas.core.timeline import Timeline
 from vistas.core.gis.extent import Extent
@@ -20,6 +22,7 @@ class Shapefile(FeatureDataPlugin):
         self._name = None
         self.metadata = None
         self.features = None
+        self._extent = None
 
     def load_data(self):
 
@@ -27,10 +30,52 @@ class Shapefile(FeatureDataPlugin):
 
         with fiona.open(self.path, 'r') as shp:
             self.metadata = shp.meta
-            self.features = [f for f in shp]
+            features = [f for f in shp]
 
-        print(self.features)
-        print(self.metadata)
+        def explode(coords):
+            """ Borrowed from sgilles - https://gis.stackexchange.com/questions/90553/fiona-get-each-feature-extent-bounds
+            Explode a GeoJSON geometry's coordinates object and yield coordinate tuples.
+            As long as the input is conforming, the type of the geometry doesn't matter."""
+            for e in coords:
+                if isinstance(e, (float, int)):
+                    yield coords
+                    break
+                else:
+                    for f in explode(e):
+                        yield f
+
+        def bbox(feature):
+            x, y = zip(*list(explode(feature['geometry']['coordinates'])))
+            return min(x), min(y), max(x), max(y)
+
+        projection = Proj(init=self.metadata['crs']['init'])
+
+        self.features = []
+        xmin, ymin, xmax, ymax = [None] * 4
+
+        for f in features:
+            if xmin is None:
+                xmin, ymin, xmax, ymax = bbox(f)
+            else:
+                _xmin, _ymin, _xmax, _ymax = bbox(f)
+                xmin, ymin, xmax, ymax = min([xmin, _xmin]), min([ymin, _ymin]), max([xmax, _xmax]), max([ymax, _ymax])
+
+            # Convert feature geometry into shapely objects
+            geometry_type = f['geometry']['type']
+
+            if geometry_type == 'Polygon':
+                try:
+                    self.features.append(Polygon(f['geometry']['coordinates']))
+                except:
+                    try:
+                        self.features.append(Polygon(f['geometry']['coordinates'][0]))
+                    except:
+                        raise TypeError("Cannot import geometry, shapefile is malformed...")
+
+            elif geometry_type == 'Point':
+                pass    # Todo - implement support for points
+
+        self._extent = Extent(xmin, ymin, xmax, ymax, projection)
 
     @property
     def data_name(self):
@@ -42,8 +87,7 @@ class Shapefile(FeatureDataPlugin):
 
     @property
     def extent(self):
-
-        return None  # Todo - construct extent from self.metadata
+        return self._extent
 
     @property
     def time_info(self):
@@ -56,13 +100,11 @@ class Shapefile(FeatureDataPlugin):
 
     @property
     def variables(self):
-
-        raise NotImplemented    # Todo - get a list of variable names from the features
+        return list(self.metadata['schema']['properties'].keys())
 
     def calculate_stats(self):
 
-        pass  # Todo - calculate stats
+        pass  # Todo - calculate stats?
 
     def get_features(self, date=None):
-
-        raise NotImplemented    # Todo - construct shapely feature set
+        return self.features
