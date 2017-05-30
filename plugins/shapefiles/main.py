8d6baa941ce252681b6722f24c0b293de99fee68
Taylor Mutch <taylormutch@gmail.com>
2017-05-29 22:00:30 -0700

    Extended shapefiles plugin.

--- plugins/shapefiles/main.py
+++ plugins/shapefiles/main.py
@@ -1,11 +1,11 @@
 import os
 import fiona
 from pyproj import Proj
-from shapely.geometry import Point, Polygon
+from shapely.geometry import Point, Polygon, MultiPolygon
 
 from vistas.core.timeline import Timeline
 from vistas.core.gis.extent import Extent
-from vistas.core.plugins.data import FeatureDataPlugin
+from vistas.core.plugins.data import FeatureDataPlugin, VariableStats, TemporalInfo
 
 
 class Shapefile(FeatureDataPlugin):
@@ -14,15 +14,15 @@ class Shapefile(FeatureDataPlugin):
     name = 'Shapefile Data Plugin'
     description = 'A plugin to read shapefiles (.shp)'
     author = 'Conservation Biology Institute'
-    extensions = [('shp', 'Shapefile'), ('json', 'GeoJSON')]
+    extensions = [('shp', 'Shapefile')]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self._name = None
         self.metadata = None
-        self.features = None
         self._extent = None
+        self._stats = None
 
     def load_data(self):
 
@@ -30,50 +30,34 @@ class Shapefile(FeatureDataPlugin):
 
         with fiona.open(self.path, 'r') as shp:
             self.metadata = shp.meta
-            features = [f for f in shp]
+            projection = Proj(init=self.metadata['crs']['init'])
 
-        def explode(coords):
-            """ Borrowed from sgilles - https://gis.stackexchange.com/questions/90553/fiona-get-each-feature-extent-bounds
-            Explode a GeoJSON geometry's coordinates object and yield coordinate tuples.
-            As long as the input is conforming, the type of the geometry doesn't matter."""
-            for e in coords:
-                if isinstance(e, (float, int)):
-                    yield coords
-                    break
+            # Determine extent
+            def explode(coords):
+                """ Borrowed from sgilles - https://gis.stackexchange.com/questions/90553/fiona-get-each-feature-extent-bounds
+                Explode a GeoJSON geometry's coordinates object and yield coordinate tuples.
+                As long as the input is conforming, the type of the geometry doesn't matter."""
+                for e in coords:
+                    if isinstance(e, (float, int)):
+                        yield coords
+                        break
+                    else:
+                        for inner_coords in explode(e):
+                            yield inner_coords
+
+            def bbox(feature):
+                x, y = zip(*list(explode(feature['geometry']['coordinates'])))
+                return min(x), min(y), max(x), max(y)
+
+            xmin, ymin, xmax, ymax = [None] * 4
+
+            for f in shp:
+                if xmin is None:
+                    xmin, ymin, xmax, ymax = bbox(f)
                 else:
-                    for f in explode(e):
-                        yield f
-
-        def bbox(feature):
-            x, y = zip(*list(explode(feature['geometry']['coordinates'])))
-            return min(x), min(y), max(x), max(y)
-
-        projection = Proj(init=self.metadata['crs']['init'])
-
-        self.features = []
-        xmin, ymin, xmax, ymax = [None] * 4
-
-        for f in features:
-            if xmin is None:
-                xmin, ymin, xmax, ymax = bbox(f)
-            else:
-                _xmin, _ymin, _xmax, _ymax = bbox(f)
-                xmin, ymin, xmax, ymax = min([xmin, _xmin]), min([ymin, _ymin]), max([xmax, _xmax]), max([ymax, _ymax])
-
-            # Convert feature geometry into shapely objects
-            geometry_type = f['geometry']['type']
-
-            if geometry_type == 'Polygon':
-                try:
-                    self.features.append(Polygon(f['geometry']['coordinates']))
-                except:
-                    try:
-                        self.features.append(Polygon(f['geometry']['coordinates'][0]))
-                    except:
-                        raise TypeError("Cannot import geometry, shapefile is malformed...")
-
-            elif geometry_type == 'Point':
-                pass    # Todo - implement support for points
+                    _xmin, _ymin, _xmax, _ymax = bbox(f)
+                    xmin, ymin, xmax, ymax = \
+                        min([xmin, _xmin]), min([ymin, _ymin]), max([xmax, _xmax]), max([ymax, _ymax])
 
         self._extent = Extent(xmin, ymin, xmax, ymax, projection)
 
@@ -91,12 +75,13 @@ class Shapefile(FeatureDataPlugin):
 
     @property
     def time_info(self):
-
-        return None  # Todo - implement ENVISION-style pattern matching for regex
+        return TemporalInfo()  # Todo - implement ENVISION-style pattern matching for time-enabled shapefiles
 
     def variable_stats(self, variable):
-
-        return None  # Todo - implement stats for each feature that we get
+        for stats in self._stats:
+            if stats[0] == variable:
+                return stats[1]
+        return None
 
     @property
     def variables(self):
@@ -104,7 +89,65 @@ class Shapefile(FeatureDataPlugin):
 
     def calculate_stats(self):
 
-        pass  # Todo - calculate stats?
+        variables = self.variables
+        all_stats = [(var, VariableStats()) for var in self.variables]
+
+        with fiona.open(self.path, 'r') as shp:
+
+            for feature in shp:
+
+                for i, var in enumerate(variables):
+                    stats = all_stats[i][1]
+                    value = feature['properties'][var]
+
+                    if isinstance(value, (int, float)):
+
+                        if stats.max_value is None:
+                            stats.max_value = stats.min_value = value
+                        else:
+                            if stats.max_value < value:
+                                stats.max_value = value
+                            elif stats.min_value > value:
+                                stats.min_value = value
+
+                    elif isinstance(value, str):
+                        misc = stats.misc
+
+                        if 'unique_values' not in misc:
+                            misc['unique_values'] = []
+
+                        misc['unique_values'].append(value)
+
+        self._stats = all_stats
 
     def get_features(self, date=None):
-        return self.features
+
+        features = []
+        with fiona.open(self.path, 'r') as shp:
+
+            for f in shp:
+
+                # Convert feature geometry into shapely objects
+                geometry_type = f['geometry']['type']
+                coords = f['geometry']['coordinates']
+
+                try:
+                    if geometry_type == 'Point':
+                        shape = Point(coords)
+
+                    elif geometry_type == 'Polygon':
+                        shape = Polygon(coords)
+
+                    elif geometry_type == 'MultiPolygon':
+                        shape = MultiPolygon(coords)
+
+                    else:
+                        continue    # Todo - handle all types?
+
+                    features.append(shape)
+
+                except:
+                    print("Failed to import geometry.")
+                    continue
+
+        return features
