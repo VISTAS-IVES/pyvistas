ffd68bac6700fb7bf102260c425f8205f0cf5dd5
Nik Molnar <nik.molnar@consbio.org>
2017-07-14 10:26:07 -0700

    Update exe build

--- source/setup.py
+++ source/setup.py
@@ -1,8 +1,6 @@
 import os
 import sys
-from distutils.extension import Extension
 
-from Cython.Build import cythonize
 from cx_Freeze import setup, Executable
 
 SOURCE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -11,7 +9,9 @@ os.environ['TCL_LIBRARY'] = r'C:\Python35\tcl\tcl8.6'
 os.environ['TK_LIBRARY'] = r'C:\Python35\tcl\tk8.6'
 
 build_options = dict(
-    packages=['asyncio', 'OpenGL', 'tkinter', 'numpy', 'pyproj', 'vistas'],
+    packages=[
+        'asyncio', 'OpenGL', 'tkinter', 'numpy', 'pyproj', 'vistas', 'netCDF4', 'netcdftime', 'fiona', 'rasterio'
+    ],
     excludes=[],
     include_files=[('../plugins', 'plugins'), ('../resources', 'resources')],
     constants='VISTAS_PROFILE="deploy"'
@@ -25,20 +25,10 @@ executables = [
     )
 ]
 
-extensions = [
-    Extension(
-        'vistas.core.encoders.wmv._wmv',
-        sources=['vistas/core/encoders/wmv/_wmv.pyx'],
-        libraries=['Mfuuid', 'Mfplat', 'Mfreadwrite'],
-        language='c++'
-    )
-]
-
 setup(
     name='VISTAS',
     version='1.0',
     description='VISTAS',
     options=dict(build_exe=build_options),
-    executables=executables,
-    ext_modules=cythonize(extensions)
+    executables=executables
 )
