c9ff273e06c9e806f4a3c52b99dcb963af77db65
Taylor Mutch <taylormutch@gmail.com>
2017-07-20 17:07:21 -0700

    Fixed buggy number entry in export options dialog, added FloatCtrl.

---
+++ source/vistas/ui/controls/float_ctrl.py
@@ -0,0 +1,42 @@
+from string import digits
+import wx
+
+
+FloatCtrlEventBase, EVT_FLOAT = wx.lib.newevent.NewEvent()
+
+
+class FloatCtrlEvent(FloatCtrlEventBase):
+    def __init__(self, value=None):
+        super().__init__(value=value)
+
+
+class FloatCtrl(wx.TextCtrl):
+    """
+    A custom text input for handling floating point values. Functions almost exactly like wx.TextCtrl, except values are
+    expected to be `float` rather than `str`.
+    """
+    def __init__(self, parent, id=wx.ID_ANY, value=0.0, pos=wx.DefaultPosition, size=wx.DefaultSize, style=0,
+                 validator=wx.DefaultValidator, name='floatctrl'):
+        super().__init__(parent, id, value=str(value), pos=pos, size=size, style=style, validator=validator, name=name)
+
+        self.Bind(wx.EVT_TEXT, self.OnText)
+
+    def OnText(self, event):
+        value = ''.join(c for c in super().GetValue() if c in digits + '.')
+        if not value:
+            value = '0'
+        self.ChangeValue(value)     # don't send another event
+
+        try:
+            wx.PostEvent(self, FloatCtrlEvent(self.GetValue()))
+        except ValueError:
+            pass
+        event.Skip()
+
+    def GetValue(self):
+        return float(super().GetValue())
+
+    def SetValue(self, value):
+        if not isinstance(value, (float, int)):
+            raise ValueError('FloatCtrl requires float values')
+        super().SetValue(str(value))
--- source/vistas/ui/windows/export_options_dialog.py
+++ source/vistas/ui/windows/export_options_dialog.py
@@ -2,6 +2,7 @@ import wx
 import wx.lib.masked.numctrl
 
 from vistas.core.timeline import Timeline
+from vistas.ui.controls.float_ctrl import FloatCtrl, EVT_FLOAT
 
 
 class ExportOptionsDialog(wx.Dialog):
@@ -23,16 +24,12 @@ class ExportOptionsDialog(wx.Dialog):
         initial_export_length = 30.0
 
         export_length_static = wx.StaticText(main_panel, wx.ID_ANY, "Length of export (in seconds):")
-        self.export_length_ctrl = wx.lib.masked.numctrl.NumCtrl(main_panel, wx.ID_ANY, value=initial_export_length,
-                                                                size=wx.Size(50, -1), allowNegative=False,
-                                                                fractionWidth=1, min=1)
+        self.export_length_ctrl = FloatCtrl(main_panel, wx.ID_ANY, value=initial_export_length, size=wx.Size(50, -1))
 
         initial_export_timestamps = Timeline.app().num_timestamps / initial_export_length
 
         export_frames_static = wx.StaticText(main_panel, wx.ID_ANY, "Timestamps per second:")
-        self.export_frames_ctrl = wx.lib.masked.numctrl.NumCtrl(main_panel, wx.ID_ANY, value=initial_export_timestamps,
-                                                                size=wx.Size(50, -1), allowNegative=False,
-                                                                fractionWidth=2, min=0)
+        self.export_frames_ctrl = FloatCtrl(main_panel, wx.ID_ANY, value=initial_export_timestamps, size=wx.Size(50, -1))
 
         self.export_frames_ctrl.Enable(enable_frames_input)
 
@@ -60,8 +57,8 @@ class ExportOptionsDialog(wx.Dialog):
         main_sizer.Add(main_panel, 1, wx.EXPAND | wx.ALL, 5)
         main_sizer.Add(self.CreateButtonSizer(wx.OK | wx.CANCEL), 0, wx.EXPAND | wx.ALL, 5)
 
-        self.export_length_ctrl.Bind(wx.EVT_TEXT, self.OnExportLengthEndInput)
-        self.export_frames_ctrl.Bind(wx.EVT_TEXT, self.OnExportFramesEndInput)
+        self.export_length_ctrl.Bind(EVT_FLOAT, self.OnExportLengthInput)
+        self.export_frames_ctrl.Bind(EVT_FLOAT, self.OnExportFramesInput)
 
         self.Fit()
 
@@ -78,11 +75,11 @@ class ExportOptionsDialog(wx.Dialog):
         val = self.export_length_ctrl.GetValue()
         return val if val > 0 else 1
 
-    def OnExportLengthEndInput(self, event):
+    def OnExportLengthInput(self, event):
         if self.export_frames_ctrl.IsEnabled():
             export_timestamps = Timeline.app().num_timestamps / self.export_length
-            self.export_frames_ctrl.ChangeValue(export_timestamps)
+            self.export_frames_ctrl.ChangeValue(str(export_timestamps))
 
-    def OnExportFramesEndInput(self, event):
+    def OnExportFramesInput(self, event):
         input_frames = float(self.export_frames_ctrl.GetValue())
-        self.export_length_ctrl.ChangeValue(Timeline.app().num_timestamps / input_frames)
+        self.export_length_ctrl.ChangeValue(str(Timeline.app().num_timestamps / input_frames))
