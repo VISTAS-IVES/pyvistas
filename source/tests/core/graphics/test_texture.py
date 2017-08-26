afc91b24378cc7f7e3a6e085073730fd21a50540
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2017-08-26 16:22:48 -0700

    Test from `MainLoop`

--- source/tests/core/graphics/test_texture.py
+++ source/tests/core/graphics/test_texture.py
@@ -10,20 +10,26 @@ vistas_app # Make the fixture import look used to IDEs
 
 
 def test_texture_with_data(vistas_app):
-    data = numpy.arange(300)
+    def test_callback():
+        data = numpy.arange(300)
 
-    with patch('{}.glTexImage2D'.format(texture.__name__)) as m:
-        t = texture.Texture(data, 10, 10)
-        assert m.called
-        assert m.call_args[0][-1] is data
-        assert m.call_args[0][3:5] == (10, 10)
+        with patch('{}.glTexImage2D'.format(texture.__name__)) as m:
+            t = texture.Texture(data, 10, 10)
+            assert m.called
+            assert m.call_args[0][-1] is data
+            assert m.call_args[0][3:5] == (10, 10)
+
+    vistas_app(test_callback)
 
 
 def test_texture_with_image(vistas_app):
-    im = Image.new('RGB', (10, 10), (0, 0, 0, 0))
+    def test_callback():
+        im = Image.new('RGB', (10, 10), (0, 0, 0, 0))
 
-    with patch('{}.glTexImage2D'.format(texture.__name__)) as m:
-        t = texture.Texture(im)
-        assert m.called
-        assert (m.call_args[0][-1].ravel() == numpy.zeros(300)).all()
-        assert m.call_args[0][3:5] == (10, 10)
+        with patch('{}.glTexImage2D'.format(texture.__name__)) as m:
+            t = texture.Texture(im)
+            assert m.called
+            assert (m.call_args[0][-1].ravel() == numpy.zeros(300)).all()
+            assert m.call_args[0][3:5] == (10, 10)
+
+    vistas_app(test_callback)
--- source/tests/fixtures.py
+++ source/tests/fixtures.py
@@ -1,5 +1,8 @@
 import os
+from typing import NamedTuple
+
 import pytest
+import sys
 import wx
 
 from vistas.ui.app import App
@@ -18,13 +21,37 @@ def generic_app():
     app.ProcessPendingEvents()
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function')
 def vistas_app():
-    source_dir = os.path.dirname(os.path.dirname(__file__))
-    os.chdir(source_dir)
+    class TestResult:
+        exc = None
 
-    app = App.get()
-    yield app
-    wx.CallAfter(app.ExitMainLoop)
-    app.MainLoop()
-    app.ProcessPendingEvents()
+    def run_test(callback):
+        source_dir = os.path.dirname(os.path.dirname(__file__))
+        os.chdir(source_dir)
+
+        excepthook = sys.excepthook
+        result = TestResult()
+
+        app = App.get()
+
+        def test_callback():
+            sys.excepthook = excepthook
+
+            try:
+                callback()
+            except Exception as e:
+                result.exc = e
+            finally:
+                app.ExitMainLoop()
+
+        timer = wx.Timer()
+        timer.Bind(wx.EVT_TIMER, lambda event: test_callback())
+        timer.Start(1, True)
+
+        app.MainLoop()
+
+        if result.exc is not None:
+            raise result.exc
+
+    return run_test
