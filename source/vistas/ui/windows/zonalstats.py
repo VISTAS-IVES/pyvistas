bada31fbc2a79691b81dfd3168fa4f7a1e75b6f6
Taylor Mutch <taylormutch@gmail.com>
2017-12-04 16:41:21 -0800

    Display stats for each zonal stats result by column.

--- source/vistas/ui/windows/zonalstats.py
+++ source/vistas/ui/windows/zonalstats.py
@@ -1,3 +1,5 @@
+import math
+
 import wx
 import wx.grid
 import wx.lib.scrolledpanel as scrolled
@@ -14,8 +16,8 @@ class ZonalStatisticsWindow(wx.Frame):
         scroll_sizer = wx.BoxSizer(wx.VERTICAL)
         scroll_panel = scrolled.ScrolledPanel(self)
         self.grid = wx.grid.Grid(scroll_panel)
-        scroll_sizer.Add(self.grid, 1, wx.EXPAND)
         scroll_panel.SetSizer(scroll_sizer)
+        scroll_sizer.Add(self.grid, 1, wx.EXPAND)
         scroll_panel.SetupScrolling()
 
         self.grid.CreateGrid(1, 2)
@@ -23,6 +25,15 @@ class ZonalStatisticsWindow(wx.Frame):
         self.grid.SetColLabelSize(0)
         self.Bind(wx.EVT_CLOSE, self.OnClose)
         self._data = []
+
+    @staticmethod
+    def _format_sig_figs(value, limit):
+        """ Format cell values to use significant figures. """
+        if not value:
+            return 0
+        power = -int(math.floor(math.log10(abs(value)))) + (limit - 1)
+        factor = (10 ** power)
+        return '{}'.format(round(value * factor) / factor)
 
     @property
     def data(self):
@@ -43,38 +54,33 @@ class ZonalStatisticsWindow(wx.Frame):
         if self._data is None:
             return
 
-        # Ensure name is first
-        labels = list(self._data[0].keys() if isinstance(self._data[0], dict) else self._data[0][0].keys())
-        if 'Name' in labels:
-            label = labels.pop(labels.index('Name'))
-            labels.insert(0, label)
+        column_headings = ['Statistic'] + [x.get('Name') for x in self._data]
+        row_headings = [y for y in self._data[0].keys() if y != 'Name']
 
-        self.grid.AppendRows(1)
-        for i, label in enumerate(labels):
-            self.grid.AppendCols(1)
-            self.grid.SetCellValue(0, i, label)
+        # Format the row and column headings
+        self.grid.AppendRows(len(row_headings) + 1)
+        self.grid.AppendCols(len(column_headings))
+        for i, name in enumerate(column_headings):
+            self.grid.SetCellValue(0, i, name)
+        for i, name in enumerate(row_headings):
+            self.grid.SetCellValue(i + 1, 0, name)
 
-        num_rows = 0
+        # Populate the table
+        sig_figs_limit = 10
+        for j, data in enumerate(self._data):
+            for i, heading in enumerate(row_headings):
+                self.grid.SetCellValue(
+                    i + 1, j + 1, self._format_sig_figs(data[heading], sig_figs_limit)
+                )
 
-        def add_rows(data):
-            nonlocal self, num_rows
-            self.grid.AppendRows(1)
-            num_rows += 1
-            for key in data.keys():
-                col = labels.index(key)
-                self.grid.SetCellValue(num_rows, col, str(data[key]))
+        # Fit the rows/columns
+        self.grid.AutoSize()
 
-        for data in self._data:
-            if isinstance(data, dict):
-                add_rows(data)
-            elif isinstance(data, list):
-                for inner_row in data:
-                    if isinstance(inner_row, dict):
-                        add_rows(inner_row)
-
-        self.grid.AutoSizeRows(setAsMin=True)
-        if not self.IsShown():
-            self.SetSize(wx.Size(400, 180))
+        # Reset the size of the scrolled window
+        size = self.grid.GetSize()
+        self.SetClientSize(size)
+        self.SetVirtualSize(size)
+        self.Fit()
 
     def Show(self, show=True):
         if not self.IsShown():
