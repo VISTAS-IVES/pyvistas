fa5599a3453cf18e9452be96540041ebd710c94b
Unknown <kenninof@gmail.com>
2018-04-27 17:27:47 -0700

    Add capability to do multiple linear regression

---
+++ source/vistas/ui/windows/linreg_dialog.py
@@ -0,0 +1,171 @@
+import wx
+
+import numpy as np
+import numpy.ma as ma
+import sklearn as sk
+import sklearn.linear_model as sklm
+
+import matplotlib as mpl
+mpl.use('WXAgg')
+import matplotlib.backends.backend_wxagg as wxagg
+
+from vistas.ui.project import Project
+from vistas.core.timeline import Timeline
+from vistas.ui.utils import get_main_window
+from vistas.ui.events import EVT_TIMELINE_CHANGED
+
+
+class LinRegDialog(wx.Frame):
+    def __init__(self, parent=None):
+        super().__init__(parent, size=(800,800))
+        self.panel = wx.Panel(self)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.panel.SetSizer(self.sizer)
+        top_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer.Add(top_sizer, 0)
+        ctl_sizer = wx.BoxSizer(wx.VERTICAL)
+        top_sizer.Add(ctl_sizer, 0)
+        ctl_sizer.Add(wx.StaticText(self.panel, -1, 'Variables:'), flag=wx.TOP|wx.LEFT|wx.RIGHT, border=20)
+
+        self.data = Project.get().all_data
+        data_choices = [n.data.data_name for n in self.data]
+
+        ctl_sizer.Add(wx.StaticText(self.panel, -1, 'Independent variable(s):'), flag=wx.TOP|wx.LEFT|wx.RIGHT, border=12)
+        self.iv_chooser = wx.ListBox(self.panel, choices=data_choices, style=wx.LB_EXTENDED)
+        ctl_sizer.Add(self.iv_chooser, flag=wx.LEFT|wx.RIGHT, border=12)
+
+        ctl_sizer.Add(wx.StaticText(self.panel, -1, 'Dependent variable'), flag=wx.TOP|wx.LEFT|wx.RIGHT, border=12)
+        self.dv_chooser = wx.Choice(self.panel, choices=['-'] + data_choices)
+        ctl_sizer.Add(self.dv_chooser, flag=wx.LEFT|wx.RIGHT, border=12)
+
+        ctl_sizer.Add(wx.StaticText(self.panel, -1, 'Plot type:'), flag=wx.TOP|wx.LEFT|wx.RIGHT, border=12)
+        self.plot_type = wx.RadioBox(self.panel, choices=['scatterplot', 'heatmap'])
+        ctl_sizer.Add(self.plot_type, flag=wx.LEFT|wx.RIGHT, border=10)
+        ctl_sizer.Add(wx.StaticText(self.panel, -1, 'Axis type:'), flag=wx.TOP|wx.LEFT|wx.RIGHT, border=12)
+        self.axis_type = wx.RadioBox(self.panel, choices=['fixed', 'adaptive'])
+        ctl_sizer.Add(self.axis_type, flag=wx.LEFT|wx.RIGHT, border=10)
+        self.Bind(wx.EVT_RADIOBOX, self.doPlot)
+
+        self.fig = mpl.figure.Figure()
+        self.canvas = wxagg.FigureCanvasWxAgg(self.panel, -1, self.fig)
+        top_sizer.Add(self.canvas, 1, wx.EXPAND)
+        self.Bind(wx.EVT_LISTBOX, self.doPlot)
+        self.Bind(wx.EVT_CHOICE, self.doPlot)
+        self.Bind(wx.EVT_CLOSE, self.onClose)
+        get_main_window().Bind(EVT_TIMELINE_CHANGED, self.doPlot)
+        self.CenterOnParent()
+        self.panel.Layout()
+        self.Show()
+
+    def getData(self, dname, data):
+        try:
+            node = data[[n.data.data_name for n in data].index(dname)]
+        except ValueError:
+            return None
+        variable = node.data.variables[0]
+        stats = node.data.variable_stats(variable)
+        date = Timeline.app().current
+        thisdata = node.data.get_data(variable, date=date)
+        return {'name': dname, 'data': thisdata, 'min': stats.min_value, 'max': stats.max_value}
+
+
+    def plotLinReg(self, iv=None, dv=None):
+        try:
+          self.fig.delaxes(self.ax)
+        except:
+          pass
+
+        # stack data and synchronize masks
+        my_data = ma.array([d['data'] for d in dv + iv])
+        my_data.mask = np.where(ma.sum(my_data.mask, axis=0), True, False)
+        my_data = np.array([d.compressed() for d in my_data])
+
+        dv_data = my_data[0]
+        dv_data = dv_data.reshape(dv_data.shape + (1,))
+        iv_data = my_data[1:].transpose()
+
+        ols = sklm.LinearRegression()
+        ols.fit(iv_data, dv_data)
+        r2 = ols.score(iv_data, dv_data)
+
+        if len(iv) == 1: # plot iff we have a single independent variable
+            self.ax = self.fig.add_subplot(111)
+            self.ax.set_xlabel(iv[0]['name'])
+            self.ax.set_ylabel(dv[0]['name'])
+            axis_type = self.axis_type.GetString(self.axis_type.GetSelection())
+            if axis_type == 'fixed':
+                self.ax.set_xlim(iv[0]['min'], iv[0]['max'])
+                self.ax.set_ylim(dv[0]['min'], dv[0]['max'])
+            self.ax.grid()
+
+            dv_plot_data = my_data[0]
+            iv_plot_data = my_data[1]
+
+            plot_type = self.plot_type.GetString(self.plot_type.GetSelection())
+            if plot_type == 'scatterplot':
+                # adjust marker size and alpha based on how many points we're plotting
+                marker_size = mpl.rcParams['lines.markersize'] ** 2
+                marker_size *= min(1, max(.12, 200 / len(iv_plot_data)))
+                alpha = min(1, max(.002, 500 / len(iv_plot_data)))
+                self.ax.scatter(iv_plot_data, dv_plot_data, s=marker_size, alpha=alpha)
+            else: # heatmap
+                bins = 200
+                heatmap, iv_edges, dv_edges = np.histogram2d(iv_plot_data, dv_plot_data, bins=bins)
+                x_min, x_max = iv_edges[0], iv_edges[-1]
+                y_min, y_max = dv_edges[0], dv_edges[-1]
+                self.ax.imshow(np.log(heatmap.transpose() + 1),
+                  extent=[x_min, x_max, y_min, y_max], cmap='Blues', origin='lower', aspect='auto')
+            # plot regression line
+            extent = [ma.min(iv_plot_data), ma.max(iv_plot_data)]
+            self.ax.plot(extent, [ols.intercept_[0] + ols.coef_[0] * x for x in extent], 'r--')
+            self.fig.tight_layout()
+            self.canvas.draw()
+
+
+        # show stats in grid
+        try:
+          self.grid.Destroy()
+        except:
+          pass
+
+        self.grid = wx.grid.Grid(self.panel, -1)
+        self.grid.CreateGrid(2 + len(iv), 2)
+        self.grid.SetRowLabelSize(0)
+        self.grid.SetColLabelSize(0)
+        self.grid.SetColFormatFloat(1, 6, 3)
+        grid_data = [['r^2', r2], ['intercept', ols.intercept_[0]]]\
+          + [['coefficient for ' + v['name'], ols.coef_[0][i]] for i,v in enumerate(iv)]
+        for r in range(2 + len(iv)):
+            for c in range(2):
+                self.grid.SetCellValue(r, c, str(grid_data[r][c]))
+                self.grid.SetReadOnly(r, c)
+        self.grid.AutoSize()
+        self.sizer.Add(self.grid, 2, flag=wx.EXPAND|wx.ALL, border=10)
+        self.panel.Layout()
+
+    def doPlot(self, event):
+        try:
+            iv_selections = [self.iv_chooser.GetString(s) for s in self.iv_chooser.GetSelections()]
+            dv_selection = self.dv_chooser.GetString(self.dv_chooser.GetSelection())
+            if dv_selection in iv_selections:
+                iv_selections.remove(dv_selection)
+            if len(iv_selections) > 0 and dv_selection != '-': # good to go
+                data = {}
+                for v, sel in zip(['iv', 'dv'], [iv_selections, [dv_selection]]):
+                    dd = []
+                    for dname in sel:
+                        try:
+                            thisdata = self.getData(dname, self.data)
+                            if thisdata is not None:
+                                dd.append(thisdata)
+                        except Exception as ex:
+                            print(ex)
+                    data[v] = dd
+                if len(data['iv']) >= 1 and len(data['dv']) >= 1:
+                    self.plotLinReg(**data)
+        finally:
+            event.Skip() # pass to next handler
+
+    def onClose(self, event):
+        get_main_window().Unbind(EVT_TIMELINE_CHANGED)
+        self.Destroy()
