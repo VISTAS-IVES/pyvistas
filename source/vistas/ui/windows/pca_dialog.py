35f925f1bcb1fb02882de62daee75b4b52b6dfdc
Caden Kesey <cjkesey@willamette.edu>
2019-07-24 16:56:21 -0700

    Fix pca dialog to plot scatterplots and update when pressing radio buttons

--- source/vistas/ui/windows/pca_dialog.py
+++ source/vistas/ui/windows/pca_dialog.py
@@ -7,6 +7,7 @@ import sklearn as sk
 import sklearn.decomposition as skd
 
 import matplotlib as mpl
+
 mpl.use('WXAgg')
 import matplotlib.backends.backend_wxagg as wxagg
 
@@ -18,7 +19,7 @@ from vistas.ui.events import EVT_TIMELINE_CHANGED
 
 class PcaDialog(wx.Frame):
     def __init__(self, parent=None):
-        super().__init__(parent, title='PCA', size=(800,800))
+        super().__init__(parent, title='PCA', size=(800, 800))
 
         self.zoom = zoom_file.zoom()
 
@@ -39,19 +40,20 @@ class PcaDialog(wx.Frame):
         zoom_sizer = wx.BoxSizer(wx.HORIZONTAL)  # Sizer for the zoom controls
         self.right_sizer = wx.BoxSizer(wx.VERTICAL)  # Sizer for the right half of the window
 
-        ctl_sizer.Add(wx.StaticText(self.panel, -1, 'Variables:'), flag=wx.TOP|wx.LEFT|wx.RIGHT, border=20)
+        ctl_sizer.Add(wx.StaticText(self.panel, -1, 'Variables:'), flag=wx.TOP | wx.LEFT | wx.RIGHT, border=20)
 
         self.data = Project.get().all_data
         data_choices = [n.data.data_name for n in self.data]
 
         self.chooser = wx.ListBox(self.panel, choices=data_choices, style=wx.LB_EXTENDED)
-        ctl_sizer.Add(self.chooser, 0, flag=wx.LEFT|wx.RIGHT|wx.BOTTOM, border=20)
+        ctl_sizer.Add(self.chooser, 0, flag=wx.LEFT | wx.RIGHT | wx.BOTTOM, border=20)
 
-        ctl_sizer.Add(wx.StaticText(self.panel, -1, 'Plot type:'), flag=wx.TOP|wx.LEFT|wx.RIGHT, border=12)
+        ctl_sizer.Add(wx.StaticText(self.panel, -1, 'Plot type:'), flag=wx.TOP | wx.LEFT | wx.RIGHT, border=12)
         self.plot_type = wx.RadioBox(self.panel, choices=['scatterplot', 'heatmap'])
-        ctl_sizer.Add(self.plot_type, flag=wx.LEFT|wx.RIGHT, border=10)
+        ctl_sizer.Add(self.plot_type, flag=wx.LEFT | wx.RIGHT, border=10)
+
         ctl_sizer.Add(wx.StaticText(self.panel, -1, 'Axis type:'), flag=wx.TOP|wx.LEFT|wx.RIGHT, border=12)
-        self.axis_type = wx.RadioBox(self.panel, choices=['fixed', 'adaptive', 'Zoom'])
+        self.axis_type = wx.RadioBox(self.panel, choices=['Fixed', 'Adaptive', 'Zoom'])
         ctl_sizer.Add(self.axis_type, flag=wx.LEFT|wx.RIGHT, border=10)
         self.Bind(wx.EVT_RADIOBOX, self.on_axis_change)
 
@@ -82,7 +84,7 @@ class PcaDialog(wx.Frame):
         top_sizer.Add(self.right_sizer)
 
         self.Bind(wx.EVT_LISTBOX, self.on_var_change)
-        self.Bind(wx.EVT_CLOSE, self.onClose)
+        self.Bind(wx.EVT_CLOSE, self.on_close)
         get_main_window().Bind(EVT_TIMELINE_CHANGED, self.on_timeline_change)
 
         # Mouse events on graph (Matplotlib events)
@@ -93,6 +95,11 @@ class PcaDialog(wx.Frame):
         self.CenterOnParent()
         self.panel.Layout()
         self.Show()
+
+    def on_plot_change(self, event):
+        self.update_graph = True
+        self.do_plot(event)
+        event.Skip()
 
     def on_axis_change(self, event):
         """Update graph when user selects a new axis type"""
@@ -199,12 +206,10 @@ class PcaDialog(wx.Frame):
         self.ax.set_ylim(zoom_values[2], zoom_values[3])
 
     def get_data(self, dname, data):
-        node = data[[n.data.data_name for n in data].index(dname)]
-
-        # try:
-        #     node = data[[n.data.data_name for n in data].index(dname)]
-        # except ValueError:
-        #     return None
+        try:
+            node = data[[n.data.data_name for n in data].index(dname)]
+        except ValueError:
+            return None
         date = Timeline.app().current
         thisdata = node.data.get_data(node.data.variables[0], date=date)
         return thisdata
@@ -248,7 +253,6 @@ class PcaDialog(wx.Frame):
         if self.update_table:
             self.create_table(n_vars, pca, var_names)
 
-
     def create_graph(self, n_vars, pca, tx_data, var_names, x_data):
         try:
           self.fig.delaxes(self.ax)
@@ -262,7 +266,7 @@ class PcaDialog(wx.Frame):
         self.ax.set_ylabel('Component 2')
 
         axis_type = self.axis_type.GetString(self.axis_type.GetSelection())
-        if axis_type == 'fixed':
+        if axis_type == 'Fixed':
             self.ax.set_xlim(-3, 3)
             self.ax.set_ylim(-3, 3)
         elif axis_type == 'Zoom':
@@ -274,15 +278,16 @@ class PcaDialog(wx.Frame):
         if plot_type == 'scatterplot':
             # adjust marker size and alpha based on # of points
             marker_size = mpl.rcParams['lines.markersize'] ** 2
-            marker_size *= min(1, max(.12, 200 / len(tx_data[:,0])))
-            alpha = min(1, max(.002, 500 / len(tx_data[:,0])))
-            self.ax.scatter(200, 100, s=marker_size, c='b', alpha=alpha)
-        else: # heatmap
+            marker_size *= min(1, max(.12, 200 / len(tx_data[:, 0])))
+            alpha = min(1, max(.002, 500 / len(tx_data[:, 0])))
+            self.ax.scatter(tx_data[:, 0], tx_data[:, 1], s=marker_size, c='b', alpha=alpha)
+        else:  # heatmap
             bins = 200
-            heatmap, x_edges, y_edges = np.histogram2d(tx_data[:,0], tx_data[:,1], bins=bins)
+            heatmap, x_edges, y_edges = np.histogram2d(tx_data[:, 0], tx_data[:, 1], bins=bins)
             x_min, x_max = x_edges[0], x_edges[-1]
             y_min, y_max = y_edges[0], y_edges[-1]
-            self.ax.imshow(np.log(heatmap.transpose() + 1), extent=[x_min, x_max, y_min, y_max], cmap='Blues', origin='lower', aspect='auto')
+            self.ax.imshow(np.log(heatmap.transpose() + 1), extent=[x_min, x_max, y_min, y_max], cmap='Blues',
+                           origin='lower', aspect='auto')
 
         # plot axes
         color = ['g', 'r', 'm', 'k', 'y']
@@ -291,10 +296,11 @@ class PcaDialog(wx.Frame):
             adata[0, n] = ma.min(x_data[:, n])
             adata[1, n] = ma.max(x_data[:, n])
             xf = pca.transform(adata)
-            self.ax.plot([xf[0,0], xf[0,1]], [xf[1,0], xf[1,1]], color[n % len(color)] + '-')
-            self.ax.text(xf[0,0], xf[1,0], var_names[n], color=color[n % len(color)])
-
+            self.ax.plot([xf[0, 0], xf[0, 1]], [xf[1, 0], xf[1, 1]], color[n % len(color)] + '-')
+            self.ax.text(xf[0, 0], xf[1, 0], var_names[n], color=color[n % len(color)])
         self.fig.tight_layout()
+
+        self.panel.Layout()
 
     def create_table(self, n_vars, pca, var_names):
         self.panel.Freeze()
@@ -304,9 +310,8 @@ class PcaDialog(wx.Frame):
           self.grid.Destroy()
         except AttributeError:
           pass
-
         self.grid = wx.grid.Grid(self.panel, -1)
-        self.grid.CreateGrid(n_vars, n_vars+1) # extra column for explained variance
+        self.grid.CreateGrid(n_vars, n_vars + 1)  # extra column for explained variance
         grid_labels = ['Expl. var.'] + var_names
         grid_data = np.concatenate(
           (np.expand_dims(pca.explained_variance_ratio_, axis=1), pca.components_),
@@ -361,6 +366,6 @@ class PcaDialog(wx.Frame):
         else:
             self.plot_adjust(*self.zoom.get_bounds_absolute())
 
-    def onClose(self, event):
+    def on_close(self, event):
         get_main_window().Unbind(EVT_TIMELINE_CHANGED)
         self.Destroy()
