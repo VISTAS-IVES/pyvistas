aef4d6e23918b525e6d967ca50a6ee52fac2d53c
Caden Kesey <cjkesey@willamette.edu>
2019-07-23 17:46:24 -0700

    Add zooming to pca window

--- source/vistas/ui/windows/pca_dialog.py
+++ source/vistas/ui/windows/pca_dialog.py
@@ -1,4 +1,5 @@
 import wx
+import vistas.ui.windows.zoom as zoom_file
 
 import numpy as np
 import numpy.ma as ma
@@ -18,6 +19,14 @@ from vistas.ui.events import EVT_TIMELINE_CHANGED
 class PcaDialog(wx.Frame):
     def __init__(self, parent=None):
         super().__init__(parent, title='PCA', size=(800,800))
+
+        self.zoom = zoom_file.zoom()
+
+        # Global variables
+        self.reset_bounds = True  # True if bounds have been reset by changing variables
+        self.update_graph = True  # True if graph needs to be updated
+        self.update_table = True  # True if data tables need to be updated
+
         self.panel = wx.Panel(self)
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.sizer.AddSpacer(5)
@@ -26,6 +35,10 @@ class PcaDialog(wx.Frame):
         self.sizer.Add(top_sizer, 0)
         ctl_sizer = wx.BoxSizer(wx.VERTICAL)
         top_sizer.Add(ctl_sizer, 0)
+
+        zoom_sizer = wx.BoxSizer(wx.HORIZONTAL)  # Sizer for the zoom controls
+        self.right_sizer = wx.BoxSizer(wx.VERTICAL)  # Sizer for the right half of the window
+
         ctl_sizer.Add(wx.StaticText(self.panel, -1, 'Variables:'), flag=wx.TOP|wx.LEFT|wx.RIGHT, border=20)
 
         self.data = Project.get().all_data
@@ -38,40 +51,181 @@ class PcaDialog(wx.Frame):
         self.plot_type = wx.RadioBox(self.panel, choices=['scatterplot', 'heatmap'])
         ctl_sizer.Add(self.plot_type, flag=wx.LEFT|wx.RIGHT, border=10)
         ctl_sizer.Add(wx.StaticText(self.panel, -1, 'Axis type:'), flag=wx.TOP|wx.LEFT|wx.RIGHT, border=12)
-        self.axis_type = wx.RadioBox(self.panel, choices=['fixed', 'adaptive'])
+        self.axis_type = wx.RadioBox(self.panel, choices=['fixed', 'adaptive', 'Zoom'])
         ctl_sizer.Add(self.axis_type, flag=wx.LEFT|wx.RIGHT, border=10)
-        self.Bind(wx.EVT_RADIOBOX, self.doPlot)
+        self.Bind(wx.EVT_RADIOBOX, self.on_axis_change)
+
+        # Blank spacer
+        ctl_sizer.AddSpacer(220)
+
+        # Zoom controls
+
+        # Zoom button for dragging a box
+        self.zoom_box = wx.Button(self.panel, label="Box")
+        zoom_sizer.Add(self.zoom_box)
+
+        self.zoom_box.Bind(wx.EVT_BUTTON, self.on_zoom_box_button)
+
+        # Zoom slider
+        self.zoom_slider = wx.Slider(self.panel, value=0, minValue=0, maxValue=49, size=(600, -1),
+                                     style=wx.SL_HORIZONTAL)
+        zoom_sizer.Add(self.zoom_slider, flag=wx.LEFT, border=10)
+
+        self.zoom_slider.Bind(wx.EVT_SCROLL, self.on_zoom_scroll)
 
 
         self.fig = mpl.figure.Figure()
         self.canvas = wxagg.FigureCanvasWxAgg(self.panel, -1, self.fig)
-        top_sizer.Add(self.canvas, 1, wx.EXPAND)
-        self.Bind(wx.EVT_LISTBOX, self.doPlot)
+
+        self.right_sizer.Add(self.canvas, 1, wx.EXPAND)
+        self.right_sizer.Add(zoom_sizer, flag=wx.EXPAND | wx.TOP | wx.LEFT | wx.RIGHT, border=10)
+        top_sizer.Add(self.right_sizer)
+
+        self.Bind(wx.EVT_LISTBOX, self.on_var_change)
         self.Bind(wx.EVT_CLOSE, self.onClose)
-        get_main_window().Bind(EVT_TIMELINE_CHANGED, self.doPlot)
+        get_main_window().Bind(EVT_TIMELINE_CHANGED, self.on_timeline_change)
+
+        # Mouse events on graph (Matplotlib events)
+        self.fig.canvas.mpl_connect('button_press_event', self.on_mouse_press)
+        self.fig.canvas.mpl_connect('button_release_event', self.on_mouse_release)
+        self.fig.canvas.mpl_connect('motion_notify_event', self.on_mouse_move)
+
         self.CenterOnParent()
         self.panel.Layout()
         self.Show()
 
-    def getData(self, dname, data):
-        try:
-            node = data[[n.data.data_name for n in data].index(dname)]
-        except ValueError:
-            return None
+    def on_axis_change(self, event):
+        """Update graph when user selects a new axis type"""
+        self.update_graph = True
+        self.do_plot(event)
+
+    def on_timeline_change(self, event):
+        """Update graph and tables when the timeline changes"""
+        self.update_graph = True
+        self.update_table = True
+        self.do_plot(event)
+
+    def on_var_change(self, event):
+        """Reset the graph and tables when a new variable is chosen to plot"""
+        self.reset_bounds = True
+        self.update_graph = True
+        self.update_table = True
+        self.do_plot(event)
+
+    def reset_graph(self, x_min, x_max, y_min, y_max):
+        """Reset the zoom controls and graph bounds"""
+        self.zoom_slider.SetValue(0)
+
+        # Set bounds to fully zoomed out
+        self.ax.set_xlim(x_min, x_max)
+        self.ax.set_ylim(y_min, y_max)
+
+        if self.zoom.square in self.ax.get_children():
+            self.zoom.square.remove()
+
+        self.reset_bounds = False
+
+        self.zoom.reset_zoom(x_min, x_max, y_min, y_max)
+
+    def on_zoom_scroll(self, event):
+        self.disable_zoom()
+        self.do_plot(event)
+
+    def disable_zoom(self):
+        """Disable the button that allows user to draw a zoom box"""
+        if self.zoom_slider.GetValue() > self.zoom.zoom_disable_value:
+            self.zoom_box.Disable()
+        else:
+            self.zoom_box.Enable()
+
+    def disable_zoom_check(self):
+        """Enable zoom controls if axis type is set to Zoom"""
+        axis_type = self.axis_type.GetString(self.axis_type.GetSelection())
+        if axis_type == 'Zoom':
+            self.zoom_slider.Enable()
+            self.disable_zoom()
+        else:
+            self.zoom_slider.Disable()
+            self.zoom_box.Disable()
+
+    def on_zoom_box_button(self, event):
+        """Allow the user to draw a box on the graph"""
+        self.zoom.zoom_box_drawing_enabled()
+
+    # MOUSE METHODS MATPLOTLIB
+
+    def on_mouse_move(self, event):
+        """Get mouse position when moving"""
+        ms = wx.GetMouseState()
+        if ms.leftIsDown:
+            self.zoom.set_mouse_diff(event.xdata, event.ydata)
+            self.do_plot(event)
+        else:
+            self.zoom.set_mouse_diff_zero()
+
+    def on_mouse_press(self, event):
+        """Get mouse position when left clicking"""
+        ms = wx.GetMouseState()
+        if ms.leftIsDown:
+            self.zoom.set_mouse_diff_zero()
+            self.zoom.set_mouse_initial(event.xdata, event.ydata)
+
+    def on_mouse_release(self, event):
+        """Get mouse position when releasing a mouse press"""
+        self.zoom.mouse_release(event.xdata, event.ydata)
+        self.do_plot(event)
+
+    # PLOTTING GRAPH
+
+    def plot_zoom_graph(self, x_min, x_max, y_min, y_max):
+        """Calculate the bounds of the graph when 'Zoom' axis type is selected"""
+
+        # If variables have been changed, graph should be reset
+        if self.reset_bounds:
+            self.reset_graph(x_min, x_max, y_min, y_max)
+
+        zoom_values = self.zoom.calculate_zoom(x_min, x_max, y_min, y_max, self.zoom_slider.GetValue())
+
+        # If box should be drawn
+        if zoom_values[4]:
+            self.ax.add_patch(self.zoom.square)
+            self.ax.figure.canvas.draw()
+
+        # Set zoom slider to given value
+        self.zoom_slider.SetValue(zoom_values[5])
+        self.disable_zoom()
+
+        self.ax.set_xlim(zoom_values[0], zoom_values[1])
+        self.ax.set_ylim(zoom_values[2], zoom_values[3])
+
+    def get_data(self, dname, data):
+        node = data[[n.data.data_name for n in data].index(dname)]
+
+        # try:
+        #     node = data[[n.data.data_name for n in data].index(dname)]
+        # except ValueError:
+        #     return None
         date = Timeline.app().current
         thisdata = node.data.get_data(node.data.variables[0], date=date)
         return thisdata
 
-    def plotPCA(self, data=None):
-        try:
-          self.fig.delaxes(self.ax)
-        except:
-          pass
+    def plot_adjust(self, x1, x2, y1, y2):
+        """Adjust the bounds of the current graph without redrawing it"""
+        axis_type = self.axis_type.GetString(self.axis_type.GetSelection())
 
+        if self.zoom.square in self.ax.get_children():
+            self.zoom.square.remove()
+
+        if axis_type == 'Zoom':
+            self.plot_zoom_graph(x1, x2, y1, y2)
+
+        self.ax.figure.canvas.draw()
+
+    def plot_pca(self, data=None):
         # normalize and reorganize data
         n_vars = len(data)
         var_names = list(data)
-        x_data = data.values() # guaranteed, per python docs, to be same order
+        x_data = data.values()  # guaranteed, per python docs, to be same order
         x_data = [(d - np.mean(d)) / np.std(d) for d in x_data]
         # synchronize masks
         n_data = ma.array(x_data)
@@ -80,7 +234,7 @@ class PcaDialog(wx.Frame):
             mask = np.where(mask, True, False)
             for d in x_data:
                 d.mask = mask
-        
+
         x_data = np.concatenate([np.expand_dims(d.compressed(), axis=0) for d in x_data])
         x_data = x_data.transpose()
 
@@ -88,6 +242,18 @@ class PcaDialog(wx.Frame):
         pca = skd.PCA()
         model = pca.fit(x_data)
         tx_data = pca.transform(x_data)
+
+        self.create_graph(n_vars, pca, tx_data, var_names, x_data)
+
+        if self.update_table:
+            self.create_table(n_vars, pca, var_names)
+
+
+    def create_graph(self, n_vars, pca, tx_data, var_names, x_data):
+        try:
+          self.fig.delaxes(self.ax)
+        except AttributeError:
+          pass
 
         # plot first two components
         self.ax = self.fig.add_subplot(111)
@@ -99,6 +265,10 @@ class PcaDialog(wx.Frame):
         if axis_type == 'fixed':
             self.ax.set_xlim(-3, 3)
             self.ax.set_ylim(-3, 3)
+        elif axis_type == 'Zoom':
+            self.plot_zoom_graph(-3,3,-3,3)
+
+        self.zoom.set_bounds_absolute(-3,3,-3,3)
 
         plot_type = self.plot_type.GetString(self.plot_type.GetSelection())
         if plot_type == 'scatterplot':
@@ -106,7 +276,7 @@ class PcaDialog(wx.Frame):
             marker_size = mpl.rcParams['lines.markersize'] ** 2
             marker_size *= min(1, max(.12, 200 / len(tx_data[:,0])))
             alpha = min(1, max(.002, 500 / len(tx_data[:,0])))
-            self.ax.scatter(tx_data[:,0], tx_data[:,1], s=marker_size, c='b', alpha=alpha)
+            self.ax.scatter(200, 100, s=marker_size, c='b', alpha=alpha)
         else: # heatmap
             bins = 200
             heatmap, x_edges, y_edges = np.histogram2d(tx_data[:,0], tx_data[:,1], bins=bins)
@@ -123,13 +293,18 @@ class PcaDialog(wx.Frame):
             xf = pca.transform(adata)
             self.ax.plot([xf[0,0], xf[0,1]], [xf[1,0], xf[1,1]], color[n % len(color)] + '-')
             self.ax.text(xf[0,0], xf[1,0], var_names[n], color=color[n % len(color)])
+
         self.fig.tight_layout()
+
+    def create_table(self, n_vars, pca, var_names):
+        self.panel.Freeze()
 
         # show stats in grid
         try:
           self.grid.Destroy()
-        except:
+        except AttributeError:
           pass
+
         self.grid = wx.grid.Grid(self.panel, -1)
         self.grid.CreateGrid(n_vars, n_vars+1) # extra column for explained variance
         grid_labels = ['Expl. var.'] + var_names
@@ -151,23 +326,40 @@ class PcaDialog(wx.Frame):
         self.sizer.AddStretchSpacer()
         self.panel.Layout()
 
-    def doPlot(self, event):
-        try: # because we want to insure that we can pass to next handler
-            selections = self.chooser.GetSelections()
-            if len(selections) >= 2:
-                v_data = {}
-                for sel in selections:
-                    dname = self.chooser.GetString(sel)
-                    try:
-                        thisdata = self.getData(dname, self.data)
-                        if thisdata is not None:
-                            v_data[dname] = thisdata
-                    except Exception as ex:
-                        print(ex)
-                if len(v_data.keys()) >= 2:
-                    self.plotPCA(data=v_data)
-        finally:
-            event.Skip() # pass to next handler
+        self.panel.Thaw()
+
+        self.update_table = False
+
+    def do_plot(self, event):
+        self.disable_zoom_check()
+
+        try:
+            if self.zoom.square in self.ax.get_children():
+                self.zoom.square.remove()
+        except AttributeError:
+            pass
+
+        if self.update_graph:
+            try: # because we want to insure that we can pass to next handler
+                selections = self.chooser.GetSelections()
+                if len(selections) >= 2:
+                    v_data = {}
+                    for sel in selections:
+                        dname = self.chooser.GetString(sel)
+                        try:
+                            thisdata = self.get_data(dname, self.data)
+                            if thisdata is not None:
+                                v_data[dname] = thisdata
+                        except Exception as ex:
+                            print(ex)
+                    if len(v_data.keys()) >= 2:
+                        self.plot_pca(data=v_data)
+            finally:
+                if isinstance(event, wx.Event):  # Check if wxPython event
+                    event.Skip()  # pass to next handler
+                self.update_graph = False
+        else:
+            self.plot_adjust(*self.zoom.get_bounds_absolute())
 
     def onClose(self, event):
         get_main_window().Unbind(EVT_TIMELINE_CHANGED)
